145dae62da59f1899b87fb0e6bd8c8a3f7cb87e6
murpheycm <cmmurphey@gmail.com>
2023-06-29 18:57:51 -0400

    Updated searchpage html and js

--- searchpage.js
+++ searchpage.js
@@ -1,1 +1,26 @@
+const api_url= 'https://api.deezer.com/radio';
 
+// Function to insert artist bio information
+
+
+// Function to insert Concerts Nearby
+
+function concertList() {
+    const concertList = document.createElement("li");
+    for (var i=0; i<=nearbyConcerts.length; i++) {
+        concertList;
+        li.appendChild();
+    }
+}
+
+
+// Function for Sample Playlist
+
+
+
+
+// Function for Similar Artists
+
+function similarArtistTile() {
+
+}
