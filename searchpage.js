79724d2337e971254fe8435c895cd6126bfb1b1d
hershRVA <herchenrother@gmail.com>
2023-07-05 12:05:03 -0400

    fixed artist search on all pages and java

--- searchpage.js
+++ searchpage.js
@@ -1,133 +1,136 @@
 var artistSearch = '';
-const artistSearchUrl = 'https://deezerdevs-deezer.p.rapidapi.com/search?q=' + artistSearch;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1f74ad6fe1msh722b05fd40167f7p168350jsn83917ae41088',
-		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-	}
+var options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '1f74ad6fe1msh722b05fd40167f7p168350jsn83917ae41088',
+    'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+  }
 };
-const searchPageUrl = document.location;
 var eventSearchBtn = document.querySelector('#event-search-btn');
-var artistSearchForm = document.querySelector('#search-artist-input');
+var artistSearchForm = document.getElementById('artist-search-form');
 var locationEl = document.getElementById('location-input');
 var endDateEl = document.getElementById('end-date');
 var startDateEl = document.getElementById('start-date');
 var artistSearchEl = document.getElementById('search-artist-input');
 
 // Function to retrieve artist information
-function retrieveArtistInfo(artistSearchEl) {
-    async function logData() {
-        const response = await fetch(artistSearchUrl, options);
-        // const result = await response.text();
-        const result = await response.json();
-        console.log(result);
-    };
-    // catch (error) {
-    //     console.error(error);
-    // }
+async function retrieveArtistInfo(artistSearch) {
+  var artistSearchUrl = 'https://deezerdevs-deezer.p.rapidapi.com/search?q=' + artistSearch;
+  var response = await fetch(artistSearchUrl, options);
+  var result = await response.json();
+  console.log(result);
 
-    var artistName = data.list[0].artist.name;
-
-    if (artistSearchEl !== artistName) {
-        console.log('artist search error');
-        alert('Error: Artist not found');
+  if (result.data.length > 0) {
+    var artistName = result.data[0].artist.name;
+    if (artistSearchEl.value !== artistName) {
+      console.log('artist search error');
+      alert('Error: Artist not found');
     } else {
-      console.log('It works!')
-    // getSimilarArtists(artistId);
-    // renderArtistInfo(artistName, data.list[0]);
-    // renderPlaylist(data.list);
+      console.log('It works!');
+      // getSimilarArtists(artistId);
+      // renderArtistInfo(artistName, result.data[0]);
+      // renderPlaylist(result.data);
     }
+  } else {
+    console.log('No artist found');
+    alert('Error: Artist not found');
+  }
 }
 
-
-
-
-
-
-
-// Event handler for searchbar function
 function handleEventSearch(event) {
-    event.preventDefault();
-  
-    var eventSearch = {
-      searchLocation: locationEl.value,
-      startDate: startDateEl.value,
-      endDate: endDateEl.value
-    };
-    saveEventHistory(eventSearch);
-  
-    locationEl.value = "";
-    startDateEl.value = "";
-    endDateEl.value = "";
-  
-    return false;
+  event.preventDefault();
+
+  var searchLocation = locationEl.value;
+  var startDate = startDateEl.value;
+  var endDate = endDateEl.value;
+
+  if (!searchLocation || !startDate || !endDate) {
+    console.error('You need a search input value!');
+    alert('Please enter a Location, Start Date, and End Date!');
+    return;
   }
-  
-  // Event handler for artist search form
-  function handleArtistSearch(event) {
+
+  var queryString = './searchpage.html?searchlocation=' + searchLocation + '&startdate=' + startDate + '&enddate=' + endDate;
+  location.assign(queryString);
+  locationEl.value = '';
+  startDateEl.value = '';
+  endDateEl.value = '';
+}
+
+function handleArtistSearch(event) {
     event.preventDefault();
   
     var artistSearch = artistSearchEl.value;
   
-    saveArtistHistory(artistSearch);
-    retrieveArtistId(artistSearch);
+    if (!artistSearch) {
+      console.error('You need a search input value!');
+      alert('Please Enter an Artist');
+      return;
+    }
   
-    artistSearchEl.value = "";
+    var queryString = './searchpage.html?artist=' + artistSearch;
   
-    return false;
+    localStorage.setItem('artistSearch', artistSearch);
+    window.location.href = queryString;
+  
+    artistSearchEl.value = '';
   }
 
-function saveEventHistory() {
-    var eventSearches = JSON.parse(localStorage.getItem('eventSearches')) || [];
-    eventSearches.push(eventSearch);
-    localStorage.setItem('eventSearches', JSON.stringify(eventSearches));
-    console.log(eventSearches);
+function saveEventHistory(searchLocation, startDate, endDate) {
+  var eventSearches = JSON.parse(localStorage.getItem('eventSearches')) || [];
+  var eventSearch = {
+    searchLocation: searchLocation,
+    startDate: startDate,
+    endDate: endDate
+  };
+  eventSearches.push(eventSearch);
+  localStorage.setItem('eventSearches', JSON.stringify(eventSearches));
+  console.log(eventSearches);
 }
 
-function saveArtistHistory() {
-    var artistSearches = JSON.parse(localStorage.getItem('artistSearches')) || [];
-    artistSearches.push(artistSearch);
-    localStorage.setItem('artistSearches', JSON.stringify(artistSearches));
-    console.log(artistSearches);
-    retrieveArtistId(artistSearch);
-
+function saveArtistHistory(artistSearch) {
+  var artistSearches = JSON.parse(localStorage.getItem('artistSearches')) || [];
+  artistSearches.push(artistSearch);
+  localStorage.setItem('artistSearches', JSON.stringify(artistSearches));
+  console.log(artistSearches);
+  retrieveArtistInfo(artistSearch);
 }
+
 
 
   
 //Event listener for search button
-if (eventSearchBtn) {
-  eventSearchBtn.addEventListener('click', handleEventSearch);
-  console.log(eventSearchBtn);
-}
+if (eventSearchBtn){
+    eventSearchBtn.addEventListener('click', handleEventSearch);
+    console.log(eventSearchBtn);
+  }
+  
+  if (artistSearchForm) {
+    artistSearchForm.addEventListener('submit', handleArtistSearch);
+  }
 
-if (artistSearchForm) {
-  artistSearchForm.addEventListener('submit', handleArtistSearch);
-  console.log(artistSearchForm);
-}
 
-function getParams() {
-    console.log(document.location)
-    if (searchPageUrl.includes('artist')) {
-        var artistSearch = document.location.search.split('=');
-        console.log(artistSearch);
-
-        saveArtistHistory(artistSearch);
-    } else if (searchPageUrl.includes('searchlocation')) {
-        var eventSearch = document.location.search.split('&');
-
-        var searchLocation = eventSearch[0].split('=').pop();
-        var startDate = eventSearch[1].split('=').pop();
-        var endDate = eventSearch[2].split('=').pop();
-        console.log(eventSearch);
-
-        saveEventHistory(eventSearch);
+  function getParams() {
+    console.log(document.location);
+    if (document.location.search.includes('artist')) {
+      var artistSearch = document.location.search.split('=').pop();
+      console.log(artistSearch);
+  
+      retrieveArtistInfo(artistSearchEl);
+      saveArtistHistory(artistSearch);
+    } else if (document.location.search.includes('searchlocation')) {
+      var eventSearchArr = document.location.search.split('&');
+  
+      var searchLocation = eventSearchArr[0].split('=').pop();
+      var startDate = eventSearchArr[1].split('=').pop();
+      var endDate = eventSearchArr[2].split('=').pop();
+      console.log(eventSearchArr);
+  
+      saveEventHistory(searchLocation, startDate, endDate);
     } else {
-        return;
+      return;
     }
-    
-}
+  }
 
 
 getParams();
